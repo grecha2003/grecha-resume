c68ed20ad3f08a429a1b4f2f74d09d4618ac353e
Eredin <davidtamliani@yandex.ru>
2021-12-12 14:18:21 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,36 +1,31 @@
-$(document).ready(function() {
+$(function(){
+  $('.sidebar_social').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+
+	$('.sidebar_social').slick({
+		prevArrow: '<button type="button" class="click-btn click-prev"><img src="images/arrow-left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="click-btn "><img src="images/arrow-right.svg" alt=""></button>',
+	});
 
 	$('.sidebar_social').magnificPopup({
+		delegate: 'a',
 		type: 'image',
-		closeOnContentClick: true,
+		tLoading: 'Loading image #%curr%...',
 		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		}
-		
-	});
-
-	$('.image-popup-fit-width').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		image: {
-			verticalFit: false
-		}
-	});
-
-	$('.image-popup-no-margins').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: false,
-		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-		image: {
-			verticalFit: true
-		},
-		zoom: {
+		gallery: {
 			enabled: true,
-			duration: 300 // don't foget to change the duration also in CSS
-		}
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
 	});
 
+	$('.menu__btn').on('click', function(){
+		$('.menu__list').toggleClass('menu__list--active')
+	})
 });
