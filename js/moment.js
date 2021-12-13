f2320b6daf7908d0761586fee867c003a8edcda7
Eredin <davidtamliani@yandex.ru>
2021-12-13 22:40:44 +0300

    update

---
+++ js/moment.js
@@ -0,0 +1,3 @@
+moment.lang('ru');
+document.getElementById("datetime").innerHTML = moment("20210801", "YYYYMMDD").fromNow("()");
+console.log(datetime);
--- js/script.js
+++ js/script.js
@@ -1,31 +1,38 @@
-$(function(){
-  $('.sidebar_social').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: false
-	});
-
-	$('.sidebar_social').slick({
-		prevArrow: '<button type="button" class="click-btn click-prev"><img src="images/arrow-left.svg" alt=""></button>',
-		nextArrow: '<button type="button" class="click-btn "><img src="images/arrow-right.svg" alt=""></button>',
-	});
-
-	$('.sidebar_social').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-	});
-
-	$('.menu__btn').on('click', function(){
-		$('.menu__list').toggleClass('menu__list--active')
-	})
+var el1 = $("#cat-eye-left"),
+  eyeBall1 = el1.find("div");
+var el2 = $("#cat-eye-right"),
+  eyeBall2 = el2.find("div");
+el1.show();
+el2.show();
+var x1 = el1.offset().left + 37,
+  y1 = el1.offset().top + 25;
+var r = 10,
+  x,
+  y,
+  x2,
+  y2,
+  isEyeProcessed = false;
+$("html").mousemove(function (e) {
+  if (!isEyeProcessed) {
+    isEyeProcessed = true;
+    var x2 = e.pageX,
+      y2 = e.pageY;
+    y =
+      (r * (y2 - y1)) /
+        Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) +
+      y1;
+    x = ((y - y1) * (x2 - x1)) / (y2 - y1) + x1;
+    eyeBall1.css({
+      marginTop: y - y1 + 1 + "px",
+      marginLeft: x - x1 + "px",
+    });
+    eyeBall2.css({
+      marginTop: y - y1 + 1 + "px",
+      marginLeft: x - x1 + "px",
+    });
+    isEyeProcessed = false;
+  }
 });
+
+
+
